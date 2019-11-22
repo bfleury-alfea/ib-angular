be2562c956b5f2f0d2462db743d8a95d040d3c8b
bfleury <bruno.fleury@alfea-consulting.com>
2019-11-22 09:17:56 +0100

    WIP

--- src/app/services/fake-api/fake-api.service.ts
+++ src/app/services/fake-api/fake-api.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {InMemoryDbService} from 'angular-in-memory-web-api';
 import {Observable} from 'rxjs';
-import {PIZZAS} from '../mocks/pizzas.mock';
-import {INGREDIENTS} from '../mocks/ingredients.mock';
+import {PIZZAS} from '../../mocks/pizzas.mock';
+import {INGREDIENTS} from '../../mocks/ingredients.mock';
 
 @Injectable({
   providedIn: 'root'
